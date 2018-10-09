9e2842042f4a277c58207ed4226f6a6dd8307cc8
kiranprajapati2719 <kiran.prajapati2719@gmail.com>
2018-10-09 13:39:03 +0545

    Fixed error in server: build  path

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 5000;
-
+const buildPath = path.join(__dirname, '..', 'build');
 const app = express();
 
-app.use(express.static('build'));
+app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join('..','build', 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
