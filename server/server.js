be2365e6651ab7129bf54d7c8d1385cbed29fe95
kiranprajapati2719 <kiran.prajapati2719@gmail.com>
2018-10-09 13:49:02 +0545

    Fixing server by editing gitignore

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const buildPath = path.join(__dirname, '..', 'build');
+
 const app = express();
 
 app.use(express.static(buildPath));
