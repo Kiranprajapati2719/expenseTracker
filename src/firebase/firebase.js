f5f9876e5cdffcdd24361ee585f5d230db04db9a
kiranprajapati2719 <kiran.prajapati2719@gmail.com>
2018-10-12 13:12:08 +0545

    Added firebase connection using config vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
-import * as firebase from 'firebase';
+import  * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_ID,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 //initialize firebase
@@ -24,7 +24,7 @@ export { firebase, database as default };
 //     expenses.push({
 //       id: childSnapshot.key,
 //       ...childSnapshot.val()
-//     });
+//     }); 
 //   });
 //   console.log(expenses);
 // });
